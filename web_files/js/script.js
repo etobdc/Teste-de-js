0307c9bb2eb850c11ba4fa23be0eeaa9da72763e
etobdc <etodalla@gmail.com>
2015-11-18 23:14:05 -0200

    Mudanças aplicadas/ Slider

---
+++ web_files/js/script.js
@@ -0,0 +1,32 @@
+//ÍNICIO CÓDIGO SLIDER
+var tempo;
+var troca = 0;
+function contaTempo() {
+	tempo = setInterval(muda_img,2000);
+	console.log(tempo);
+}
+function muda_img(){
+	troca++;
+	if(troca == 1){
+		document.getElementById("img1").style.visibility = "visible";
+		document.getElementById("img1").style.opacity = "1";
+		document.getElementById("img3").style.visibility = "";
+
+		document.getElementById("img3").style.opacity = "0";
+	}else if(troca == 2){
+		document.getElementById("img1").style.visibility = "visible";
+		document.getElementById("img2").style.opacity = "1";
+		document.getElementById("img2").style.visibility = "";
+        
+        document.getElementById("img1").style.opacity = "0";
+
+	}else if(troca == 3){
+		document.getElementById("img2").style.visibility = "visible";
+		document.getElementById("img3").style.opacity = "1";
+		document.getElementById("img3").style.visibility = "";
+
+		document.getElementById("img2").style.opacity = "0";
+		troca = 0;
+	}
+}
+//FIM CÓDIGO SLIDER
