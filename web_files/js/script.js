496375def407ff5d1cc8f14feb4953a784a79f0f
etobdc <etodalla@gmail.com>
2015-11-22 01:36:15 -0200

    Complementos em todas as funções add/ slides,ordenar e misturar em funcionamento

--- web_files/js/script.js
+++ web_files/js/script.js
@@ -1,34 +1,113 @@
 //ÍNICIO CÓDIGO SLIDER
 var tempo;
 var troca = 0;
+var conta = 0;
+var setTempo = 2000;
 function contaTempo() { 
-	tempo = setInterval(muda_img,2000);
-	console.log(tempo);
+	tempo = setInterval(muda_img,setTempo);
+
 }
-function muda_img(){
+var foo = 'undefined';
+function muda_img(x){
+	setTempo = 5000;
+	clearInterval(tempo);
+	tempo = setInterval(muda_img,setTempo);
 	troca++;
-	if(troca == 1){
-		document.getElementById("img1").style.visibility = "visible";
-		document.getElementById("img1").style.opacity = "1";
-		document.getElementById("img3").style.visibility = "";
+	conta++;
+	if(typeof x == 'undefined'){
+		if(troca == 1){
+			document.getElementById("img1").style.visibility = "visible";
+			document.getElementById("load").style.display = "none";
+			document.getElementById("img1").style.opacity = "1";
+			if(conta > 1){
+				document.getElementById("img3").style.visibility = "";
 
-		document.getElementById("img3").style.opacity = "0";
-	}else if(troca == 2){
-		document.getElementById("img1").style.visibility = "visible";
-		document.getElementById("img2").style.opacity = "1";
-		document.getElementById("img2").style.visibility = "";
+				document.getElementById("img3").style.opacity = "0";
+			}
+			document.getElementById("circ_1").style.backgroundColor = "red";
+			document.getElementById("circ_3").style.backgroundColor = "#9A9797";
+		}else if(troca == 2){
+			document.getElementById("img2").style.visibility = "visible";
+			document.getElementById("img2").style.opacity = "1";
+			document.getElementById("img1").style.visibility = "";
 
-		document.getElementById("img1").style.opacity = "0";
+			document.getElementById("img1").style.opacity = "0";
 
-	}else if(troca == 3){
-		document.getElementById("img2").style.visibility = "visible";
-		document.getElementById("img3").style.opacity = "1";
-		document.getElementById("img3").style.visibility = "";
+			document.getElementById("circ_2").style.backgroundColor = "red";
+			document.getElementById("circ_1").style.backgroundColor = "#9A9797";
 
-		document.getElementById("img2").style.opacity = "0";
-		troca = 0;
+		}else if(troca == 3){
+			document.getElementById("img3").style.visibility = "visible";
+			document.getElementById("img3").style.opacity = "1";
+			document.getElementById("img2").style.visibility = "";
+
+			document.getElementById("img2").style.opacity = "0";
+			document.getElementById("circ_3").style.backgroundColor = "red";
+			document.getElementById("circ_2").style.backgroundColor = "#9A9797";
+			troca = 0;
+		}
+	}else{
+		if(x == 1){
+			console.log(x);
+			document.getElementById("img1").style.visibility = "visible";
+			document.getElementById("load").style.display = "none";
+			document.getElementById("img1").style.opacity = "1";
+
+			document.getElementById("img3").style.visibility = "";
+			document.getElementById("img3").style.opacity = "0";
+
+			document.getElementById("img2").style.visibility = "";
+			document.getElementById("img2").style.opacity = "0";
+
+			document.getElementById("circ_1").style.backgroundColor = "red";
+			document.getElementById("circ_2").style.backgroundColor = "#9A9797";
+			document.getElementById("circ_3").style.backgroundColor = "#9A9797";
+			clearInterval(tempo);
+			tempo = setInterval(muda_img,setTempo);
+			troca = 1;
+		}else if(x == 2){
+
+			console.log(x);
+			document.getElementById("img2").style.visibility = "visible";
+			document.getElementById("img2").style.opacity = "1";
+
+			document.getElementById("img1").style.visibility = "";
+			document.getElementById("img1").style.opacity = "0";
+
+			document.getElementById("img3").style.visibility = "";
+			document.getElementById("img3").style.opacity = "0";
+
+			document.getElementById("circ_2").style.backgroundColor = "red";
+			document.getElementById("circ_1").style.backgroundColor = "#9A9797";
+			document.getElementById("circ_3").style.backgroundColor = "#9A9797";
+			clearInterval(tempo);
+			tempo = setInterval(muda_img,setTempo);
+			troca = 2;
+
+		}else if(x == 3){
+
+			console.log(x);
+			document.getElementById("img3").style.visibility = "visible";
+			document.getElementById("img3").style.opacity = "1";
+
+			document.getElementById("img2").style.visibility = "";
+			document.getElementById("img2").style.opacity = "0";
+
+			document.getElementById("img1").style.visibility = "";
+			document.getElementById("img1").style.opacity = "0";
+
+			document.getElementById("circ_3").style.backgroundColor = "red";
+			document.getElementById("circ_1").style.backgroundColor = "#9A9797";
+			document.getElementById("circ_2").style.backgroundColor = "#9A9797";
+			clearInterval(tempo);
+			tempo = setInterval(muda_img,setTempo);
+			troca = 0;
+
+		}
 	}
+	
 }
+
 //FIM CÓDIGO SLIDER
 //ÍNICIO CÓDIGO TABS
 function troca_tab(x){
@@ -84,6 +163,14 @@ function troca_tab(x){
 }
 //FIM CÓDIGO TABS
 //ÍNICIO DE ORDENAR
+function SomenteNumero(e){
+	var tecla=(window.event)?event.keyCode:e.which;   
+	if((tecla>47 && tecla<58)) return true;
+	else{
+		if (tecla==8 || tecla==0) return true;
+		else  return false;
+	}
+}
 function ordem(x){
 	function ordenar(a, b){
   return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
@@ -165,19 +252,25 @@ function misturar(x){
 		var numero_5 = document.getElementById("numero_5").value;
 
 		if(numero_1 != '' && numero_2 != '' && numero_3 != '' && numero_4 != '' && numero_5 != ''){
+			if(numero_1 <= 100 && numero_2 <= 100 && numero_3 <= 100 && numero_4 <= 100 && numero_5 <= 100){
 
-			var n_total = numero_1+','+numero_2+','+numero_3+','+numero_4+','+numero_5;
-		console.log(n_total);
+				var n_total = numero_1+','+numero_2+','+numero_3+','+numero_4+','+numero_5;
+				console.log(n_total);
 
 
-			var numeros_2 = n_total.split(",");
+				var numeros_2 = n_total.split(",");
 
-			console.log(numeros_2);			
+				console.log(numeros_2);			
 
-			var resultado_2 = (numeros_2.sort(desordenar)); 
-			document.getElementById("resu_2").innerHTML = resultado_2;
-			document.getElementById("resu_2").style.padding = "0.5em";
-			document.getElementById("resu_2").style.boxShadow = "-4px 8px 25px 2px rgba(50, 50, 50, 0.63)";
+				var resultado_2 = (numeros_2.sort(desordenar)); 
+				document.getElementById("resu_2").innerHTML = resultado_2;
+				document.getElementById("resu_2").style.padding = "0.5em";
+				document.getElementById("resu_2").style.boxShadow = "-4px 8px 25px 2px rgba(50, 50, 50, 0.63)";
+			}else{
+				document.getElementById("resu_2").innerHTML = 'Os números devem ser menores ou iguais a 100';
+				document.getElementById("resu_2").style.padding = "0.5em";
+				document.getElementById("resu_2").style.boxShadow = "-4px 8px 25px 2px rgba(50, 50, 50, 0.63)";
+			}
 		}else{
 			document.getElementById("resu_2").innerHTML = 'Por favor,Preencha todos os campos';
 			document.getElementById("resu_2").style.padding = "0.5em";
